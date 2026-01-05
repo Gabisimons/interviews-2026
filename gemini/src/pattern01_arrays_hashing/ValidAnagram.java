5ed17c9daab5ec18578202f83362cdbac787dea4
Gabi Simons <gabisimons@Gabis-MacBook-Pro.local>
2026-01-05 12:32:12 +0200

    TwoSum

--- gemini/src/pattern01_arrays_hashing/ValidAnagram.java
+++ gemini/src/pattern01_arrays_hashing/ValidAnagram.java
@@ -16,7 +16,21 @@ public class ValidAnagram {
 
     public static boolean isAnagram(String s, String t) {
         // --- כאן תכתוב את הפתרון שלך ---
-        
-        return false; // זמני
+        if (s.length() != t.length()) {
+            return false;            
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
+        for (int value : map.values()) {
+            if (value != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
